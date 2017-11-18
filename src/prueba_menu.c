5a46d47d82ee6ac9fb5b5981df7652eeb350b7e0
electrototo <cristobal@liendo.net>
2017-11-17 22:55:44 -0600

    codigo para probar el registro de un usuario

--- src/prueba_menu.c
+++ src/prueba_menu.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <menus.h>
+#include <utilities.h>
 
 int main() {
-    menu_principal();
+    char usuario[50], pwd[50], direccion[50];
+    int fecha[3];
+
+    menu_agregar_usuario(usuario, pwd, fecha, direccion);
+
+    printf("Bienvenido de vuelta, %s.\n", usuario);
+    printf("Tu contraseña es: %s\n", pwd);
 
     return 0;
 }
