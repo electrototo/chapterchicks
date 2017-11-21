6561e46f0d3125bff39746aaa568f8eb4bf71a30
lennygin <32081397+lennygin@users.noreply.github.com>
2017-11-20 19:57:03 -0600

    empezar con ifs de menus

--- src/prueba_menu.c
+++ src/prueba_menu.c
@@ -3,24 +3,41 @@
 #include <utilities.h>
 
 int main() {
-    char usuario[50], pwd[50], direccion[50];
-    int fecha[3];
-
+  char usuario[50], pwd[50], direccion[50], correo[254], password1[50], password2[50], nombre_libro [100],nombre_autor[50],categoria[50];
+  int fecha[3], isbn, costo_libro,opcion_principal, opcion_acceso_admin, opcion_admin;
 
     menu_principal();
 
+    if (opcion_principal==1){
+      menu_acceso (correo, password1, password2);
+    }
+
+    else {
     menu_agregar_usuario (usuario, pwd, fecha, direccion);
+    }
 
     printf("Bienvenido de vuelta, %s.\n", usuario);
-    printf("Tu contraseña es: %s\n", pwd);
 
-    //    menu_acceso (correo, password1, password2);
+    //    printf("Tu contraseña es: %s\n", pwd);
 
+    //aqui debería hacerse una validación para ver
+    //si el usuario es admin o no.
+
+    //en este caso haré que sí lo sea
     menu_administrador_como ();
 
-    menu_administrador_general ();
+    if(opcion_acceso_admin==1){
+      menu_administrador_general ();
+    }
 
+    else {
     menu_usuario ();
+    }
+
+    if (opcion_admin==3){
+    menu_registrar_libro (nombre_libro,nombre_autor,categoria, isbn, costo_libro);
+    }
+
 
     return 0;
 }
