71e01df25ca5aea8482920cc0ae5016505cae0e7
lennygin <32081397+lennygin@users.noreply.github.com>
2017-11-20 18:54:03 -0600

    agregar funciones faltantes de menus

--- src/prueba_menu.c
+++ src/prueba_menu.c
@@ -6,10 +6,21 @@ int main() {
     char usuario[50], pwd[50], direccion[50];
     int fecha[3];
 
-    menu_agregar_usuario(usuario, pwd, fecha, direccion);
+
+    menu_principal();
+
+    menu_agregar_usuario (usuario, pwd, fecha, direccion);
 
     printf("Bienvenido de vuelta, %s.\n", usuario);
     printf("Tu contraseña es: %s\n", pwd);
+
+    //    menu_acceso (correo, password1, password2);
+
+    menu_administrador_como ();
+
+    menu_administrador_general ();
+
+    menu_usuario ();
 
     return 0;
 }
