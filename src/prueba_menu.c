2a040e993f07ffa1ccb2718db4e6ab763ff41bd4
lennygin <32081397+lennygin@users.noreply.github.com>
2017-11-23 01:40:14 -0600

    mas formato y corchetes

--- src/prueba_menu.c
+++ src/prueba_menu.c
@@ -9,14 +9,14 @@ int main() {
     char isbn[14];
 
     int fecha[3], opcion_principal, opcion_acceso_admin, opcion_admin;
-    float costo_libro;
+    float *costo_libro;
 
     creditos();
     
     menu_principal();
 
     if (opcion_principal==1) {
-        menu_acceso(correo, password1, password2);
+        menu_acceso(correo, password1);
     }
 
     else {
@@ -24,63 +24,64 @@ int main() {
     }
 
     printf("Bienvenido de vuelta, %s.\n", usuario);
-
-   
+    
+    
     
     //aqui debería hacerse una validación para ver
     //si el usuario es admin o no, por el momento lo dejaré así
     int sino;
-    printf("¿Eres admin?\n[1]Si\n[2]No\n");
+    printf("¿Eres admin? Nota*: Esta validación se realiza de manera automática\n[1]Si\n[2]No\n");
     scanf("%d", &sino);
-
-
+    
+    
     
     if (sino==1){
+
       menu_administrador_como ();
       
-      if(opcion_acceso_admin == 1){
-        menu_administrador_general();
-      }
-      
-      else {
-        menu_usuario ();
-	
-      }
-      
-      if (opcion_admin==3){
-	
-      }
-      
-      if (opcion_admin==1){
-	printf("\nInforme de libros en préstamo\n");
-      }
-      else if (opcion_admin==2){
-	printf("\nInforme de usuarios y libros en préstamo\n");
-      }
-      else if (opcion_admin==3){
-	
-        menu_registrar_libro(nombre_libro, nombre_autor, categoria, isbn, &costo_libro);
-      }
- 
-    else if (opcion_admin==4){
-      printf("\nDar de baja a un usuario\n");
-      printf("Usuario: \n");
+      if(opcion_acceso_admin == 1)
+	{	  
+	  menu_administrador_general();
+	  
+	  if (opcion_admin == 1){
+	    printf("\nInforme de libros en préstamo\n");
+	  }
+	  
+	  else if (opcion_admin==2){
+	    printf("\nInforme de usuarios y libros en préstamo\n");
+	  }
+	  
+	  else if (opcion_admin==3){
+	    menu_registrar_libro (nombre_libro,nombre_autor,categoria,isbn, costo_libro);
+	  } 
+	  
+	  else if (opcion_admin==4){
+	    printf("\nDar de baja a un usuario\n");
+	    printf("Usuario: \n");
+	  }
+	  
+	  else if (opcion_admin==5){
+	    printf("\nCategoría mas popular: ");
+	    printf("\nAutor mas popular: ");
+	    printf("\nTítulo mas popular: ");
+	  }
+	  
+	  else if (opcion_admin==6){
+	    printf("\nAyuda\n");
+	  }
+	  
+	  else if (opcion_admin>=7){
+	    return 0;
+	  }
+	}
     }
-    else if (opcion_admin==5){
-      printf("\nCategoría mas popular: ");
-      printf("\nAutor mas popular: ");
-      prinf("\nTítulo mas popular: ");
+    
+    else if (opcion_acceso_admin ==2){
+      menu_usuario ();
     }
-    else if (opcion_admin==6){
-      printf("\nAyuda\n");
-    }
-    else if (opcion_admin>=7){
-      return 0;
-    }
-
-
-    }   
-    creditos();
+    
+    //creditos();
     
     return 0;    
-}
+    
+}    
