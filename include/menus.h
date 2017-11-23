d96c6fc3945104061476f0fe49fdc229a13df2fd
electrototo <cristobal@liendo.net>
2017-11-22 20:49:51 -0600

    Modificar parametros de funciones

--- include/menus.h
+++ include/menus.h
@@ -12,9 +12,9 @@
     #define MENUS_H
 
     int menu_principal();
-    int menu_agregar_usuario(char *nombre_usuario, char *contrasena,
+    void menu_agregar_usuario(char *nombre_usuario, char *contrasena,
         int *fecha, char *direccion);
-    int menu_acceso(char *correo, char *password1, char *password2);
+    void menu_acceso(char *correo, char *password1);
     int menu_administrador_como();
     int menu_administrador_general();
     int menu_usuario();
