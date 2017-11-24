50fe5a3fee38887ad92509265bf8cbcefc213b37
electrototo <cristobal@liendo.net>
2017-11-24 07:31:31 -0600

    formato

--- src/prueba_menu.c
+++ src/prueba_menu.c
@@ -14,90 +14,80 @@ int main()
 
     creditos();
     
-    menu_principal();
+    opcion_principal = menu_principal();
 
-    if (opcion_principal==1) {
+    if (opcion_principal == 1) {
         menu_acceso(correo, password1);
     }
 
     else {
-        menu_agregar_usuario (usuario, pwd, fecha, direccion);
+        menu_agregar_usuario(usuario, pwd, fecha, direccion);
     }
 
-    
     //solo deberia imprimirse esto si el usuario
     //ya tiene cuenta
     printf("Bienvenido de vuelta, %s.\n", usuario);
-    
-    
     
     //aqui debería hacerse una validación para ver
     //si el usuario es admin o no, por el momento lo dejaré así
     int sino;
     printf("¿Eres admin? Nota*: Esta validación se realiza de manera automática\n[1]Si\n[2]No\n");
     scanf("%d", &sino);
-    getchar ();
+    getchar();
     
     
-    if (sino==1){
+    if (sino == 1){
+        menu_administrador_como();
+    
+        if(opcion_acceso_admin == 1) {
+            menu_administrador_general();
+        
+            switch (opcion_admin) {
+                
+                case 1: 
+                    printf("\nInforme de libros en préstamo\n");
+                    break;
+                    
+                case 2:
+                    printf("\nInforme de usuarios y libros en préstamo\n");
+                    break;
+                    
+                case 3:
+                    menu_registrar_libro (nombre_libro,nombre_autor,categoria,isbn, costo_libro);
+                    break;
+                    
+                case 4:
+                    printf("\nDar de baja a un usuario\n");
+                    printf("Usuario: \n");
+                    break;
+                    
+                case 5:
+                    printf("\nCategoría mas popular: ");
+                    printf("\nAutor mas popular: ");
+                    printf("\nTítulo mas popular: ");
+                    break;
+                    
+                case 6:
+                    printf("\nAyuda\n");
+                    break;
+                    
+                case 7:
+                    return 0;
+                    break;
 
-      menu_administrador_como();
-    
-      if(opcion_acceso_admin == 1)
-	{	  
-	  menu_administrador_general();
-	  
-	  switch (opcion_admin)
-	    {
-	      
-	    case 1: 
-	      printf("\nInforme de libros en préstamo\n");
-	      break;
-	      
-	    case 2:
-	      printf("\nInforme de usuarios y libros en préstamo\n");
-	      break;
-	      
-	    case 3:
-	      menu_registrar_libro (nombre_libro,nombre_autor,categoria,isbn, costo_libro);
-	      break;
-	      
-	    case 4:
-	      printf("\nDar de baja a un usuario\n");
-	      printf("Usuario: \n");
-	      break;	  
-	      
-	    case 5:
-	      printf("\nCategoría mas popular: ");
-	      printf("\nAutor mas popular: ");
-	      printf("\nTítulo mas popular: ");
-	      break;	      
-	      
-	    case 6:
-	      printf("\nAyuda\n");
-	      break;
-	      
-	    case 7:
-	      return 0;
-	      break;
-
-	    default:
-	      break;
-	    }
-	}
-	  
-      else if (opcion_acceso_admin ==2){
-	menu_usuario();
-      }
+                default:
+                    break;
+            }
+        }
+        else if (opcion_acceso_admin == 2){
+            menu_usuario();
+        }
     }
 
     else if (sino ==2){
-      menu_usuario ();
+        menu_usuario ();
     }
 
-//me hace un error que no se arreglar
-//    creditos();
-    
     return 0;    
     
 }    
