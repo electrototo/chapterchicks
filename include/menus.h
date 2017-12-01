b2c61dcd58908f3ec6f38215aab1b28dd442219a
lennygin <32081397+lennygin@users.noreply.github.com>
2017-12-01 10:47:48 -0600

    cambiar parámetro de menus-h

--- include/menus.h
+++ include/menus.h
@@ -18,7 +18,7 @@
     int menu_administrador_como();
     int menu_administrador_general();
     int menu_usuario();
-    void menu_registrar_libro(char *nombre_libro, char *nombre_autor, char *categoria, char *isbn, float *costo_libro);
+void menu_registrar_libro(char *nombre_libro, char *nombre_autor, char *categoria, char *isbn10, char isbn13, float *costo_libro, int *a_pub);
     void creditos();
     void menu_informe_prestamos();
     void menu_informe_usuarios();
