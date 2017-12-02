a7863203d4dbcc7a9353fb2947880daf6039f933
electrototo <cristobal@liendo.net>
2017-12-01 22:35:28 -0600

    Arreglar confusion de tipos de variables

--- include/menus.h
+++ include/menus.h
@@ -12,14 +12,18 @@
     #define MENUS_H
 
     int menu_principal();
+
     void menu_agregar_usuario(char *nombre_usuario, char *contrasena,
-                  int *fecha, char *direccion, char *email, char *credito);
+        int *fecha, char *direccion, char *email, char *credito);
+
     void menu_acceso(char *email, char *password1);
     int menu_administrador_como();
     int menu_administrador_general();
     int menu_usuario();
-    void menu_registrar_libro(char *titulo, int *autor, int *categoria,
-		  char *isbn10, char *isbn13, float *costo_libro, int *a_pub, char *editorial);
+
+    void menu_registrar_libro(char *titulo, char *autor, char *categoria,
+        char *isbn10, char *isbn13, float *costo_libro, int *a_pub, char *editorial);
+
     void creditos();
     void menu_informe_prestamos();
     void menu_informe_usuarios();
